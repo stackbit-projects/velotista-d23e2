d971a27736d4b08850252e1229b8e3dc3425f4b7
Stackbit Code Editor <support@stackbit.com>
2021-02-05 11:06:38 +0000

    content.js: changed

--- src/components/content.js
+++ src/components/content.js
@@ -18,16 +18,7 @@ const Content = () => (
         gridTemplateColumns: "repeat(auto-fit, minmax(240px, 340px))",
       }}
     >
-      <div>
-        <h3>What you need to Start</h3>
-        <p style={{ color: COLORS.gray }}>
-          Includes plugins for analytics, building sitemaps, and optimizing
-          images
-        </p>
-      </div>
-      <div>
-        <img src={feature} alt="a blank card floating over colorful graphics" />
-      </div>
+      
     </content>
   </div>
 )
